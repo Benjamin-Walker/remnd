fca135129efe29baab38137df2d286553cf1021c
Benjamin Walker <mail@benwalker.co.uk>
2025-09-13 07:51:54 +0100

    Preparing for 0.1.0 release

--- remnd/__init__.py
+++ remnd/__init__.py
@@ -1,3 +1,9 @@
+from importlib.metadata import version, PackageNotFoundError
+
 __all__ = ["__version__"]
-__version__ = "0.1.0"
+
+try:
+    __version__ = version("remnd")
+except PackageNotFoundError:  # dev/editable installs
+    __version__ = "0.0.0"
 
